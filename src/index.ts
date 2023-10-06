3efbd77e0fa6d82966081b6bf8bfaf67134bf05d
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-10-06 19:00:36 +0530

    feat: add basic auth

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hi there');
+import express, { Request, Response } from 'express';
+import authRoutes from './routes/auth';
+import homeRoutes from './routes/home';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['key'] }));
+
+app.use('/', homeRoutes);
+app.use('/', authRoutes);
+
+app.listen(3000, () => console.log('Listening on port 3000'));
